e0c852a5bdbbd8dd7a1b61f7b8ac4bd2856d6292
ale_maci <alessandromaci96@gmail.com>
2022-09-13 16:30:57 +0200

    adding the reservation function

--- CRW-EthOnline-smart-contract/contracts/CRWCP.sol
+++ CRW-EthOnline-smart-contract/contracts/CRWCP.sol
@@ -9,6 +9,7 @@ contract CRWCP is ERC20, AccessControl {
     bytes32 public constant RESTAURANT = keccak256("RESTAURANT");
     // percentage of the total sale amount to be converted in the token. It is as set as a constant. If we want to have the ability to change it, then no constant and a new function should be created for changing this
     uint16 public constant percentage = 5;
+    mapping(address => mapping(address => uint)) reservation;
 
     constructor() ERC20("CRW CP", "CRW CP") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -23,11 +24,20 @@ contract CRWCP is ERC20, AccessControl {
         _grantRole(RESTAURANT, _newRestaurant);
     }
 
+    /// @dev reserving a restaurant appointment and saving it as an entry
+    function reserve(address _restaurant, uint256 _timestamp) public {
+        reservation[msg.sender][_restaurant] = _timestamp;
+    }
+
     /// @dev verifying customers' sale and transferring the CRW CP token to the customer in proportion to the sale
     function verify(address _customer, uint256 _amount)
         public
         onlyRole(RESTAURANT)
     {
+        require(
+            reservation[_customer][msg.sender] != 0,
+            "No reservation exists"
+        );
         uint256 tokenTransfer = (_amount * percentage) / 100;
         _mint(_customer, tokenTransfer);
     }
